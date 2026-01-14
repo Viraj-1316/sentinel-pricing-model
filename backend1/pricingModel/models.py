e22062a6f5e2ea3982ad105a63df093fd3d17c9a
Viraj Rudrawar <rudrawarviraj1751@gmail.com>
2026-01-14 12:29:38 +0530

    conflict resolved

--- backend1/pricingModel/models.py
+++ backend1/pricingModel/models.py
@@ -4,4 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Pricing(models.Model):
+    cammera = models.IntegerField()
+    procesorer = models.IntegerField()
+    server = models.IntegerField()
+    ai_features = models.CharField(max_length=100)
+    total_costing = models.IntegerField()
     
+    def __str__(self):
+        return self. ai_features
+
+class UserPricing(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    cammera = models.IntegerField()
+    procesorer = models.IntegerField()
+    server = models.IntegerField()
+    ai_features = models.CharField(max_length=100)
+    total_costing = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+         
+  
