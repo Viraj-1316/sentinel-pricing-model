91343ab3fdb64f13d06b392696f768d5cc2a82fa
Viraj Rudrawar <rudrawarviraj1751@gmail.com>
2026-01-15 14:19:46 +0530

    conflicts resolved

--- backend1/backend/settings.py
+++ backend1/backend/settings.py
@@ -95,19 +95,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-<<<<<<< HEAD
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sentinel_db',
-        'USER': 'sentinel_user',
-        'PASSWORD': 'Sentinel@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-=======
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -118,17 +105,12 @@ DATABASES = {
 #         'PORT': '5432',
 #     }
 # }
->>>>>>> 15392913 (save changes)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pricing_model',
         'USER': 'viraj',
-<<<<<<< HEAD
-        'PASSWORD': 'viraj123',
-=======
         'PASSWORD': 'viraj123',  # replace this
->>>>>>> 15392913 (save changes)
         'HOST': 'localhost',
         'PORT': '5432',
     }
