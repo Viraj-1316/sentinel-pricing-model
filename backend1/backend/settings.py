8bdadac39832bd27b9b5b5cacdd831d99d42d7bb
Viraj Rudrawar <rudrawarviraj1751@gmail.com>
2026-02-03 12:27:08 +0530

    changes saved

--- backend1/backend/settings.py
+++ backend1/backend/settings.py
@@ -118,27 +118,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'pricing_model',
-#         'USER': 'viraj',
-#         'PASSWORD': 'viraj123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sentinel_db',
-        'USER': 'sentinel_user',
-        'PASSWORD': 'Sentinel@123',
+        'NAME': 'pricing_model',
+        'USER': 'viraj',
+        'PASSWORD': 'viraj123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sentinel_db',
+#         'USER': 'sentinel_user',
+#         'PASSWORD': 'Sentinel@123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
